fd6d17a306b69e19dc92de7bc256b5739f9f08b7
andrijana <andrijana.ojdanic@tutanota.com>
2023-10-20 02:00:04 +0200

    Define uploadToNexus

---
+++ vars/uploadToNexus.groovy
@@ -0,0 +1,10 @@
+def call(Map config = [:]){
+
+    withCredentials([usernameColonPassword(
+                    credentialsId: 'nexus-credentials',
+                    variable: 'NEXUS_USERPASS')]) {
+        sh """
+            curl -v --user ${NEXUS_USERPASS} --upload-file ${config.artifact} ${config.nexusRepositoryURL}
+        """
+    }
+}
