7644a26c41f4acbcfc66a1801ab2007a163adf8e
andrijana <andrijana.ojdanic@tutanota.com>
2023-10-23 23:50:29 +0200

    Don't use string interpolation

--- vars/nexusUtils.groovy
+++ vars/nexusUtils.groovy
@@ -3,7 +3,7 @@ import utils.GlobalVars
 def upload(Map config = [:]){
 
     withCredentials([usernameColonPassword(
-                    credentialsId: "${GlobalVars.NEXUS_CREDENTIALS_ID}",
+                    credentialsId: GlobalVars.NEXUS_CREDENTIALS_ID,
                     variable: 'NEXUS_USERPASS')]) {
         sh """
             curl -v --user ${NEXUS_USERPASS} --upload-file ${config.artifact} ${GlobalVars.NEXUS_URL}
