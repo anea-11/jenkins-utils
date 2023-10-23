99083ee80aabd51e275246eb91cb26907efe1064
andrijana <andrijana.ojdanic@tutanota.com>
2023-10-23 22:01:52 +0200

    Move nexus URL to global vars

---
+++ src/utils/GlobalVars.groovy
@@ -0,0 +1,7 @@
+package utils 
+
+class GlobalVars {
+
+    public static String NEXUS_URL = 'http://18.156.155.64:8081/repository/encoder-app-snapshot/';
+
+}
--- vars/nexusUtils.groovy
+++ vars/nexusUtils.groovy
@@ -1,10 +1,12 @@
+import utils.GlobalVars
+
 def upload(Map config = [:]){
 
     withCredentials([usernameColonPassword(
                     credentialsId: 'nexus-credentials',
                     variable: 'NEXUS_USERPASS')]) {
         sh """
-            curl -v --user ${NEXUS_USERPASS} --upload-file ${config.artifact} ${config.nexusRepositoryURL}
+            curl -v --user ${NEXUS_USERPASS} --upload-file ${config.artifact} ${GlobalVars.NEXUS_URL}
         """
     }
 }
