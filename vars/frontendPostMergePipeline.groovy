a3193bccdb95d245e7c25e7ad9b1720d892acf1c
andrijana <andrijana.ojdanic@tutanota.com>
2023-10-26 15:59:27 +0200

    Add frontend post merge pipeline

---
+++ vars/frontendPostMergePipeline.groovy
@@ -0,0 +1,50 @@
+import utils.GlobalVars
+import utils.Version
+
+def call(Map config = [:]){
+
+    def frontendAppImage
+    def appVersion
+
+    node {
+        try {
+            stage('Checkout') {
+                checkout scm
+
+                def versionString = readFile 'version.txt'
+                appVersion = new Version(versionString, "${BRANCH_NAME}", "${BUILD_ID}")
+            }
+
+            stage('Bump version') {
+                appVersion = bumpVersion(appVersion: appVersion,
+                                versionFile: 'version.txt')
+            }
+
+            stage('Build app Docker image') {
+                frontendAppImage = frontendBuildUtils.buildAppDockerImage()
+            }
+
+            stage('Sanity container tests') {
+                echo 'Mimicking sanity container tests'
+            }
+
+            stage ('Push app docker image') {
+                def imageTag = "${GlobalVars.FRONTEND_APP_NAME}-${appVersion}"
+                dockerHubUtils.pushImage(image: frontendAppImage, tag: imageTag)
+            }
+        }
+
+        catch(Exception e) {
+            currentBuild.result = 'FAILURE'
+            error("Pipeline failed: ${e.message}")
+        }
+
+        finally {
+            cleanWs()
+            try {
+                sh "docker rmi -f \$(docker images ${GlobalVars.ENCODING_DOCKER_REGISTRY} -q)"
+            }
+            catch(Exception e) {}
+        }
+    }
+}
