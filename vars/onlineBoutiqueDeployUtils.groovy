8ee564df2d01acac9b4b8fb55ff6a97d74250631
andrijana <andrijana.ojdanic@tutanota.com>
2023-11-05 17:53:43 +0100

    Add pipeline for deploying online boutique app to eks

---
+++ vars/onlineBoutiqueDeployUtils.groovy
@@ -0,0 +1,19 @@
+import utils.GlobalVars
+
+def deployToEKS(Map config = [:]){
+
+    withCredentials([usernamePassword(credentialsId: GlobalVars.AWS_CREDENTIALS_ID,
+                        passwordVariable: 'aws_pass',
+                        usernameVariable: 'aws_user')]) {
+        sh """
+            export AWS_ACCESS_KEY_ID=${aws_user}
+            export AWS_SECRET_ACCESS_KEY=${aws_pass}
+            export AWS_DEFAULT_REGION=${config.aws_region}
+
+            aws eks update-kubeconfig --name ${config.aws_eks_cluster_name}
+
+            ansible-playbook playbooks/deploy-online-boutique.yaml \
+                -e k8s_config_version=${config.k8s_config_version} \
+        """
+    }
+}
