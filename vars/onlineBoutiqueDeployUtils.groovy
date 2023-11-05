4b53511e51fe02caadd26b2447c301339a4c87a8
andrijana <andrijana.ojdanic@tutanota.com>
2023-11-05 22:05:09 +0100

    Fix string interpolation

--- vars/onlineBoutiqueDeployUtils.groovy
+++ vars/onlineBoutiqueDeployUtils.groovy
@@ -6,17 +6,17 @@ def deployToEKS(Map config = [:]){
                         passwordVariable: 'aws_pass',
                         usernameVariable: 'aws_user')]) {
 
-        sh """
+        sh '''
             export AWS_ACCESS_KEY_ID=$aws_user
             export AWS_SECRET_ACCESS_KEY=$aws_pass
-            export AWS_DEFAULT_REGION=${config.aws_region}
+            export AWS_DEFAULT_REGION=$config.aws_region
             export KUBECONFIG=\$(pwd)/kubeconfig
             export HOME=\$(pwd)
 
-            aws eks update-kubeconfig --name ${config.aws_eks_cluster_name}
+            aws eks update-kubeconfig --name $config.aws_eks_cluster_name
 
             ansible-playbook playbooks/deploy-online-boutique.yaml \
-                -e k8s_config_version=${config.k8s_config_version} \
-        """
+                -e k8s_config_version=$config.k8s_config_version \
+        '''
     }
 }
