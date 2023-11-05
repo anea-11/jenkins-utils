dac1ff8d74c23d1576048befeea3e2cfaeef3ccf
andrijana <andrijana.ojdanic@tutanota.com>
2023-11-05 22:16:36 +0100

    test

--- vars/onlineBoutiqueDeployUtils.groovy
+++ vars/onlineBoutiqueDeployUtils.groovy
@@ -6,18 +6,19 @@ def deployToEKS(Map config = [:]){
                         passwordVariable: 'aws_pass',
                         usernameVariable: 'aws_user')]) {
 
+        def aws_region=config.aws_region
+
         sh '''
             export AWS_ACCESS_KEY_ID=$aws_user
             export AWS_SECRET_ACCESS_KEY=$aws_pass
-            echo \$AWS_ACCESS_KEY_ID
-            export AWS_DEFAULT_REGION=$config.aws_region
+            export AWS_DEFAULT_REGION=$aws_region
             export KUBECONFIG=\$(pwd)/kubeconfig
             export HOME=\$(pwd)
 
-            aws eks update-kubeconfig --name $config.aws_eks_cluster_name
+            aws eks update-kubeconfig --name ${config.aws_eks_cluster_name}
 
             ansible-playbook playbooks/deploy-online-boutique.yaml \
-                -e k8s_config_version=$config.k8s_config_version \
+                -e k8s_config_version=${config.k8s_config_version} \
         '''
     }
 }
