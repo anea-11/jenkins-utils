e07eb1d6d3f9df15243794d9c9a128853d646f17
andrijana <andrijana.ojdanic@tutanota.com>
2024-02-05 22:44:41 +0100

    Update nexus url

--- src/utils/GlobalVars.groovy
+++ src/utils/GlobalVars.groovy
@@ -2,10 +2,10 @@ package utils
 
 class GlobalVars {
 
-    public static String NEXUS_URL = 'http://18.156.155.64:8081';
+    public static String NEXUS_URL = 'http://52.50.97.95:8081';
     public static String NEXUS_CREDENTIALS_ID = 'nexus-credentials';
     public static String DOCKERHUB_REGISTRY_URL =  'https://index.docker.io/v1/';
-    public static String DOCKERHUB_CREDENTIALS_ID = 'dockerhub_credentials';
+    public static String DOCKERHUB_CREDENTIALS_ID = 'dockerhub-credentials';
     public static String ENCODER_APP_NAME = 'encoder-x265'
     public static String ENCODING_DOCKER_REGISTRY = 'anea11/encoding'
     public static String ENCODER_JENKINS_BUILD_DIR = 'x265-jenkins-build'
